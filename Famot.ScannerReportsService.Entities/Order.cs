438f4eea067b0e85d7a2a5e695336a7837f1798c
CORP\wolskik <krzysztof.wolski@dmgmori.com>
2017-06-05 10:24:40 +0200

    SR-1 Add relationships fields.

--- Famot.ScannerReportsService.Entities/Order.cs
+++ Famot.ScannerReportsService.Entities/Order.cs
@@ -1,7 +1,15 @@
-﻿namespace Famot.ScannerReportsService.Entities
+﻿using System.Collections.Generic;
+
+namespace Famot.ScannerReportsService.Entities
 {
     public class Order : BaseEntity
     {
+        public Order()
+        {
+            ScannerFiles = new List<ScannerFile>();
+        }
         public int OrderID { get; set; }
+
+        public virtual ICollection<ScannerFile> ScannerFiles { get; set; }
     }
 }
--- Famot.ScannerReportsService.Entities/ScannerFile.cs
+++ Famot.ScannerReportsService.Entities/ScannerFile.cs
@@ -3,7 +3,10 @@
     public class ScannerFile : BaseEntity
     {
         public int ScannerFileID { get; set; }
+        public int OrderID { get; set; }
         public string FileName { get; set; }
         public byte[] File { get; set; }
+
+        public virtual Order Order { get; set; }
     }
 }
